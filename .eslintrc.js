b2c1e40f1e31300e3ec3d1059c7a96a2ff5c59b3
milkfish <i@mxd.moe>
2025-07-21 14:38:34 +0800

    add eslint configuration and scripts for linting TypeScript code

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+  ],
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+}; 
